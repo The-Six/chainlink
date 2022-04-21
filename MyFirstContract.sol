16fd5f3a4fa14c33a418220cfa1c219eee32f70e
The-Six <ijazdriscoll@gmail.com>
2022-04-21 19:16:18 +0100

    a getNumber function has been added so that I can view the change in state, the contract is complete. Deployment is successful on remix with a test evm

--- MyFirstContract.sol
+++ MyFirstContract.sol
@@ -7,3 +7,10 @@ contract MyFirstContract {
     function changeNumber(uint256 _num) public {
         number = _num;
     } //this function is public and can be called by anyone(an automatic getter function is generated)
+    
+    function getNumber() public view returns (uint256){
+        return number;
+    } //this function is used to view the change, it key to see that it is view (read only, no gas cost associated with calling it)
+}
+
+//the contract is complete, I have deployed this successfully on remix with a test evm. I will try deploy it to my metamask tomorrow. 
