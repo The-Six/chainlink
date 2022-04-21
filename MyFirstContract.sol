28a11d184ac703b59c89b26dfe2d0bcaf88f5248
The-Six <ijazdriscoll@gmail.com>
2022-04-21 19:09:33 +0100

    a change number function has been added to the contract, additionally there is no param set to cap ammount declared

--- MyFirstContract.sol
+++ MyFirstContract.sol
@@ -1,2 +1,9 @@
 pragma solidity ^0.8.7; //This declares the version my contract will be compiled against.
-
+contract MyFirstContract {
+ 
+    uint256 number;
+ 
+ 
+    function changeNumber(uint256 _num) public {
+        number = _num;
+    } //this function is public and can be called by anyone(an automatic getter function is generated)
