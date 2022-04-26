ee485fae11f2c2b6ce57cac1ff38fd17452534de
The-Six <ijazdriscoll@gmail.com>
2022-04-26 22:35:45 +0100

    I have added a constructor to the contract

--- PriceConsumer.sol
+++ PriceConsumer.sol
@@ -1,2 +1,17 @@
 pragma solidity ^0.8.7;
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+AggregatorV3Interface internal priceFeed; //Internal means that this is only accessed within the contract.
+    /**
+     * Network: Kovan
+     * Aggregator: ETH/USD
+     * Address: 0x9326BFA02ADD2366b30bacB125260Af641031331
+     */
+    constructor() public {
+        priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
+    } // A constructor is a function that gets executed only once upon deployment of contract.
+ 
+    /**
+     * Returns the latest price
+     */
+
